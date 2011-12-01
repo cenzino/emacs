9748079d07476351aab639fb53730111b79866ca
Vincenzo Petrungaro <cenzinoos@gmail.com>
2011-12-01 19:02:01 +0100

    Aggiunto il nr di colonna

--- custom.el
+++ custom.el
@@ -3,7 +3,12 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
+ '(fringe-mode (quote (1 . 1)) nil (fringe))
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(text-mode-hook (quote (turn-on-auto-fill text-mode-hook-identify))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
